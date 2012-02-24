fdc7ea4ac3b45ea09b05ebc032b93f35b1d8d8f8
Daniel Bingham <dbingham@theroadgoeson.com>
2012-02-24 13:12:03 -0500

    Issue #3

--- application/controllers/CustomerController.php
+++ application/controllers/CustomerController.php
@@ -5,21 +5,44 @@ class CustomerController extends Zend_Controller_Action {
 
     // {{{ registerAction()
 
+    /**
+    *  Register a new customer user. Take a redirect as a parameter.
+    * If the redirect is "checkout", send the newly registered user
+    * back to the checkout step.  
+    *
+    */
     public function registerAction() {
         if(Zend_Auth::getInstance()->hasIdentity()) {
             throw new RuntimeException('Attempting to register when already logged in!');
         }
 
+        $redirect = $this->getRequest()->getParam('redirect', 'dashboard');
+
         if($this->getRequest()->isPost()) {
             $translator = new Application_Service_Translator_Customer();
-            $user = new Application_Model_User();
+            if($redirect == 'checkout') {
+                $session = new Zend_Session_Namespace('cart');
+                $user = $session->customer;
+            } else {
+                $user = new Application_Model_User();
+            }
+
             if(!$translator->translate($user, $this->getRequest()->getPost())) {
                 $this->view->errors = $translator->getErrors(); 
             } else {
                 $persistor = new Application_Model_Persistor_User();
                 $persistor->save($user);
-        
-                $this->_helper->redirector('dashboard'); 
+     
+                // Log in this user. 
+                Zend_Auth::getInstance()->getStorage()->write($user);
+
+ 
+                if($redirect == 'checkout') {
+                    $session->customer = $user; 
+                    $this->_helper->redirector('payment', 'cart'); 
+                } else { 
+                    $this->_helper->redirector('dashboard'); 
+                }
             }
         } 
 
@@ -32,10 +55,15 @@ class CustomerController extends Zend_Controller_Action {
         if(!Zend_Auth::getInstance()->hasIdentity()) {
             throw new RuntimeException('We should not be here with out an identity.');
         }
-
+        
+        $this->view->pendingOrder = Application_Model_Query_Order::getInstance()->findOne(array(
+                                                        'userID'=>Zend_Auth::getInstance()->getIdentity()->id,
+                                                        'confirmed'=>1,
+                                                        'filled'=>0));
     }
 
     // }}}
+
 }
 
 ?>
--- application/controllers/GrowerController.php
+++ application/controllers/GrowerController.php
@@ -2,18 +2,6 @@
 
 class GrowerController extends Zend_Controller_Action {
 
-    // {{{ dashboardAction()
-
-    public function dashboardAction() {
-        if(!Zend_Auth::getInstance()->hasIdentity() || Zend_Auth::getInstance()->getIdentity()->isGrower === false) {
-            return $this->_helper->redirector('browse', 'product');
-        }
-
-        $this->view->farms = Application_Model_Query_Farm::getInstance()->fetchAll(array('userID'=>Zend_Auth::getInstance()->getIdentity()->id));       
-
-    }
-
-    // }}}
     // {{{ registerAction() 
 
     public function registerAction() {
@@ -31,6 +19,9 @@ class GrowerController extends Zend_Controller_Action {
                 $persistor = new Application_Model_Persistor_User();
                 $persistor->save($user);
 
+                // Log in this user. 
+                Zend_Auth::getInstance()->getStorage()->write($user);
+
                 return $this->_helper->redirector('dashboard', 'grower');
             }
         }
@@ -38,6 +29,30 @@ class GrowerController extends Zend_Controller_Action {
     }
 
     // }}}
+    // {{{ dashboardAction()
+
+    public function dashboardAction() {
+        if(!Zend_Auth::getInstance()->hasIdentity() || Zend_Auth::getInstance()->getIdentity()->isGrower === false) {
+            return $this->_helper->redirector('browse', 'product');
+        }
+
+        $this->view->farms = Application_Model_Query_Farm::getInstance()->fetchAll(array('userID'=>Zend_Auth::getInstance()->getIdentity()->id));       
+        $this->view->ordersPending = count(Application_Model_Query_Order::getInstance()->getPendingOrdersForGrower(Zend_Auth::getInstance()->getIdentity()));
+    }
+
+    // }}}
+    // {{{ pendingAction()
+
+    public function pendingAction() {
+        if(!Zend_Auth::getInstance()->hasIdentity() || Zend_Auth::getInstance()->getIdentity()->isGrower === false) {
+            return $this->_helper->redirector('browse', 'product');
+        } 
+        $this->view->orders = Application_Model_Query_Order::getInstance()->getPendingOrdersForGrower(Zend_Auth::getInstance()->getIdentity());
+        $this->view->grower = Zend_Auth::getInstance()->getIdentity();
+    }
+
+    // }}}
+
 }
 
 ?>
